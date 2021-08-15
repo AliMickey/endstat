96f63c2b95684f53d265d6965afbb13d3b493fd3
Micky <ad312@pm.me>
2021-08-15 21:18:33 +1000

    prepare for docker host

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t endstat .
-docker run -d -p 8003:80 --name=endstat --restart unless-stopped -v $PWD:/app -v $PWD/instance/endstat.sqlite:/app/instance/endstat.sqlite endstat
+docker run -d -p 8000:80 --name=endstat --restart unless-stopped -v $PWD:/app -v $PWD/instance/endstat.sqlite:/app/instance/endstat.sqlite endstat
