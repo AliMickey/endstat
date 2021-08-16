491861144c3dbfdaf0a94fb2e7581b71b70963a8
Micky <ad312@pm.me>
2021-08-16 18:41:40 +1000

    temp ui setup

--- endstat/main.py
+++ endstat/main.py
@@ -10,3 +10,7 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/table')
+def table():
+    return render_template('table.html')
