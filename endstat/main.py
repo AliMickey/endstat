4f0839257035abd9c16ae501b3c1cfd542020059
Micky <ad312@pm.me>
2021-10-06 18:43:20 +1100

    implement dashboard

--- endstat/main.py
+++ endstat/main.py
@@ -2,12 +2,13 @@ from flask import (
     Blueprint, g, redirect, render_template, request, url_for
 )
 from datetime import datetime
+import json
 
 # App imports
 from endstat.auth import login_required
 from endstat.db import get_db
-import endstat.notifications as notif
 from endstat.profile import getAlertIcon
+from endstat.websites import loadScanResults
 
 bp = Blueprint('main', __name__)
 
@@ -21,20 +22,59 @@ def index():
 @login_required
 def dashboard():
     db = get_db()
-    dashboardDict = {}
-    # dashboardDB = db.execute('SELECT , FROM  WHERE user_id = ?', 
-	# 	(userID,)).fetchall() 
+    totalSecurityRating = 0
+    totalSecurityCounter = 0
+    latestScan = [None, None]
+    previousScans = []
+    soonestExpiry = [None, None]
+    websiteUsage = 0
 
-    # dashboardLogDB = db.execute('SELECT , FROM  WHERE user_id = ?', 
-	# 	(userID,)).fetchall() 
+    # Get all websits for user
+    userWebsitesDb = db.execute('SELECT id, domain FROM websites WHERE user_id = ?', (g.user['id'],)).fetchall()
+    websiteUsage = len(userWebsitesDb)
+    for website in userWebsitesDb:
+        # Get past 5 logs for website
+        websiteLogs = db.execute('SELECT datetime(date_time), status, general, ssl, safety, ports from website_log WHERE website_id = ? ORDER BY datetime(date_time) DESC Limit 5', (website[0],)).fetchall()
+        for log in websiteLogs:
+            # Get latest scan out of all logs
+            if latestScan[0] is not None:
+                if log[0] > latestScan[0][0]:
+                    previousScans.append([latestScan[0], latestScan[1]])
+                    latestScan[0] = log
+                    latestScan[1] = website[0]
+                else:
+                    previousScans.append([log, website[0]])
+            else:
+                latestScan[0] = log
+                latestScan[1] = website[0]
 
-    # for row in dashboardDB:
-    #     message, dateTime, type = row
-    #     icon = getAlertIcon(type)
-    #     conv_date = datetime.datetime.strptime(dateTime, "%Y-%m-%d %H:%M:%S").date().strftime("%d/%m/%Y")
-    #     dashboardDict[message] = [conv_date, type, icon]
+            # Get soonest cert expiry of all logs
+            ssl = json.loads(log['ssl']) 
+            currentExpiry = int(ssl['sslExpiry'])
+            if soonestExpiry[0] is None:
+                soonestExpiry[0] = currentExpiry
+                soonestExpiry[1] = website[1]
+            else:
+                if soonestExpiry[0] > currentExpiry:
+                    soonestExpiry[0] = currentExpiry
+                    soonestExpiry[1] = website[1]
 
-    return render_template('main/dashboard.html')
+            # Get safety rating for log and add to master var
+            safety = json.loads(log['safety'])
+            totalSecurityRating += int(safety['securePercentage'])
+            totalSecurityCounter += 1
+
+    # Sort previous 4 scans and pass as dict
+    sortedPreviousScans = sorted(previousScans, key=lambda t: datetime.strptime(t[0][0], '%Y-%m-%d %H:%M:%S'))
+    next4Scans = []
+    for log in sortedPreviousScans:
+        next4Scans.append(loadScanResults(log[0], log[1]))
+    next4Scans.reverse()
+
+    # Get average security rating
+    averageRatingPercentage = int(totalSecurityRating/totalSecurityCounter)  
+
+    return render_template('main/dashboard.html', latestScan=loadScanResults(latestScan[0], latestScan[1]), averageRatingPercentage=averageRatingPercentage, soonestExpiry=soonestExpiry, websiteUsage=websiteUsage, next4Scans=next4Scans[:4])
 
 # View for privacy policy
 @bp.route('/privacy-policy')
@@ -55,7 +95,6 @@ def page_not_found(e):
 @bp.app_errorhandler(403)
 def authorisation_error(e):
     return render_template('error/403.html'), 403
-
 
 # Supply each page view with unread user alerts.
 @bp.app_context_processor
