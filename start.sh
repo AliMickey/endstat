d7f306244e4ce9e1d234b0715e2e2c521f5240a0
Micky <ad312@pm.me>
2021-10-20 22:48:59 +1100

    remove redundant mapping

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ else
     echo "Container is not running"
 fi
 docker build -t endstat .
-docker run -d -p 8000:80 --name=endstat --restart unless-stopped -v $PWD:/app -v $PWD/instance/endstat.sqlite:/app/instance/endstat.sqlite endstat
+docker run -d -p 8000:80 --name=endstat --restart unless-stopped -v $PWD:/app endstat
